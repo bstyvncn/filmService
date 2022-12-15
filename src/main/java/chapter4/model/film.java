140943db4bff08d30683341b6b6364302882ad8a
User <besty.vincen@gmail.com>
2022-12-15 21:06:34 +0700

    filmService

---
+++ src/main/java/chapter4/model/film.java
@@ -0,0 +1,14 @@
+package chapter4.model;
+import lombok.Data;
+import javax.persistence.*;
+
+@Entity
+@Data
+public class film {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer filmID;
+
+    private String namaFilm;
+    private String statusTayang;
+}
